bff5e066a6eda9f42d43d439556a93e8321ef620
Kirialaa <Daw2300@o2.pl>
2017-09-12 12:29:02 +0200

    Update Biblioteka.cpp

--- Biblioteka.cpp
+++ Biblioteka.cpp
@@ -1,3 +1,21 @@
 #include <iostream>
 #include <string>
 #include "Biblioteka.h"
+
+using namespace std;
+
+string rozpoznaj_komende(int liter, string komenda, string tekst)
+{
+  int i=0;
+  int pomocnicza=0, bufor_pomocniczy=0;
+  string bufor;
+  int dlugosc;
+  dlugosc=tekst.length();
+  bufor_pomocniczy=liter;
+  while(i<dlugosc)
+  {
+    
+    
+    
+   }
+}
